37beb5a77803363596e92600e9f9c06bdc46d336
moomanmen <35313363+moomanmen@users.noreply.github.com>
2018-03-07 16:16:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log('JavaScript loaded');
 	function printDom(){
 		var all = document.getElementsByTagName("*");
 		for(i=0; i < all.length; i++){
-			console.log(all[i].textContent);
+			console.log(all[i]);
 		}		
 	}
 
